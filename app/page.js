d486b763bd3e04aab3bcc46714ad30f6a3035cd5
Md Azad Hossain <sokalazad@gmail.com>
2025-02-02 01:30:28 +0100

    design the services a little bit and added a path for details

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Services from "./homeComponents/Services";
 
 export default function Home() {
   return (
-    <>
+    <section>
       <Services></Services>
-    </>
+    </section>
   );
 }
