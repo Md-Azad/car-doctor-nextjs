a30fa1641f08ea66674de02599ec2a97f22015b6
Md Azad Hossain <sokalazad@gmail.com>
2025-02-03 23:52:14 +0100

    added form in register page

--- app/register/page.js
+++ app/register/page.js
@@ -1,9 +1,73 @@
+import Button from "@/components/Button";
+import Image from "next/image";
 import React from "react";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+  };
   return (
-    <div>
-      <h1>Register page.</h1>
+    <div className="my-12 w-11/12 md:w-10/12 flex  mx-auto">
+      <figure className="md:w-1/2">
+        <Image
+          src={"/assets/images/login/login.svg"}
+          width={400}
+          height={300}
+          className="hidden md:inline-block"
+        />
+      </figure>
+      <form
+        onSubmit={handleSubmit}
+        className="card-body w-full md:w-1/2 border-2 rounded-lg"
+      >
+        <h1 className="text-center text-2xl font-bold text-purple-700">
+          Register
+        </h1>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control mt-6">
+          <button>Register</button>
+        </div>
+      </form>
     </div>
   );
 };
---
+++ components/Button.jsx
@@ -0,0 +1,6 @@
+"use client";
+const Button = ({ children }) => {
+  return <button type="submit">{children}</button>;
+};
+
+export default Button;
